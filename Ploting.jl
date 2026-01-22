ac36f551a207d19c34d6ca140dceba0a6402be4a
Shabir Ahmad <121692534+Shabirahmad427@users.noreply.github.com>
2026-01-22 16:01:53 -0300

    Update Ploting.jl

--- Ploting.jl
+++ Ploting.jl
@@ -22,10 +22,10 @@ water_gromacs_1 = ComplexMixtures.load("../../proj_GH27/analyses/results-sucrose
 plot!(water_gromacs_1.d,water_gromacs_1.kb,label="gromacs_water 1.0")
 
 
-plot!(water_amber_0_5.d,sucrose_amber_0_5.kb./1000,label="amber 0.5")
-plot!(water_gromacs_0_5.d,sucrose_gromacs_0_5.kb./1000,label="gromacs 0.5")
-plot!(water_amber_1.d,sucrose_amber_1.kb./1000,label="amber 1.0")
-plot!(water_gromacs_1.d,sucrose_gromacs_1.kb./1000,label="gromacs 1.0")
+plot!(sucrose_amber_0_5.d,sucrose_amber_0_5.kb./1000,label="amber 0.5")
+plot!(sucrose_gromacs_0_5.d,sucrose_gromacs_0_5.kb./1000,label="gromacs 0.5")
+plot!(sucrose_amber_1.d,sucrose_amber_1.kb./1000,label="amber 1.0")
+plot!(sucrose_gromacs_1.d,sucrose_gromacs_1.kb./1000,label="gromacs 1.0")
 plot!(water_amber_0_5.d,sucrose_amber_0_5.kb./1000,label="amber_water 0.5")
 plot!(water_gromacs_0_5.d,sucrose_gromacs_0_5.kb./1000,label="gromacs_water 0.5")
 plot!(water_amber_1.d,sucrose_amber_1.kb./1000,label="amber_water 1.0")
