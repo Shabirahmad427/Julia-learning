b18539e39e33e45693c07db92acc6b8720a082c0
Shabir Ahmad <121692534+Shabirahmad427@users.noreply.github.com>
2026-01-22 15:46:55 -0300

    Update Ploting.jl

--- Ploting.jl
+++ Ploting.jl
@@ -1,19 +1,32 @@
 julia> using ComplexMixtures
 
 julia> using Plots
+julia> sucrose_amber_0.5 = ComplexMixtures.load("../analyses/results-sucrose_0.5M.json")
+julia> plot(sucrose_amber_0.5.d,sucrose_amber_0.5.kb,label="amber 0.5")
+julia> sucrose_gromacs_0.5 = ComplexMixtures.load("../../proj_GH27/analyses/results-sucrose_0.5M.json")
+julia> plot!(sucrose_gromacs_0.5.d,sucrose_gromacs_0.5.kb,label="gromacs 0.5")
 
-julia> plot(sucrose.d,sucrose.kb,label="gromacs 0.5")
+julia> sucrose_amber_1.0 = ComplexMixtures.load("../analyses/results-sucrose_1.0M.json")
+julia> plot!(sucrose_amber_1.0.d,sucrose_amber_1.0.kb,label="amber 1.0")
+julia> sucrose_gromacs_1.0 = ComplexMixtures.load("../../proj_GH27/analyses/results-sucrose_1.0M.json")
+julia> plot!(sucrose_gromacs_1.0.d,sucrose_gromacs_1.0.kb,label="gromacs 1.0")
 
-julia> sucrose_amber = ComplexMixtures.load("../../analyses/results-sucrose_0.5M.json")
+julia> water_amber_0.5 = ComplexMixtures.load("../analyses/results-sucrose_0.5M.json")
+julia> plot!(water_amber_0.5.d,water_amber_0.5.kb,label="amber 0.5")
+julia> water_gromacs_0.5 = ComplexMixtures.load("../../proj_GH27/analyses/results-sucrose_0.5M.json")
+julia> plot!(water_gromacs_0.5.d,water_gromacs_0.5.kb,label="gromacs 0.5")
 
-julia> sucrose_amber = ComplexMixtures.load("../../05_BgAgal27Suc/analyses/results-sucrose_0.5M.json")
+julia> water_amber_1.0 = ComplexMixtures.load("../analyses/results-sucrose_1.0M.json")
+julia> plot!(water_amber_1.0.d,water_amber_1.0.kb,label="amber 1.0")
+julia> water_gromacs = ComplexMixtures.load("../../proj_GH27/analyses/results-sucrose_1.0M.json")
+julia> plot!(water_gromacs_1.0.d,water_gromacs_1.0.kb,label="gromacs 1.0")
 
-julia> plot!(sucrose_amber.d,sucrose_amber.kb,label="amber 0.5")
+julia> plot!(sucrose_gromacs_0.5.d,sucrose_gromacs_0.5.kb./1000,label="gromacs 0.5")
 
-julia> sucrose = ComplexMixtures.load("../analyses/results-sucrose_0.5M.json")^C
+julia> plot!(sucrose_amber_0.5.d,sucrose_amber_0.5.kb./1000,label="amber 0.5")
 
-julia> plot(sucrose.d,sucrose.kb./1000,label="gromacs 0.5")
+julia> plot!(sucrose_gromacs_1.0.d,sucrose_gromacs_1.0.kb./1000,label="gromacs 1.0")
 
-julia> plot!(sucrose_amber.d,sucrose_amber.kb./1000,label="amber 0.5")
+julia> plot!(sucrose_amber_1.0.d,sucrose_amber_1.0.kb./1000,label="amber 1.0")
 
 julia> 
